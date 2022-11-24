65a3c29fceaa4f7ea3a00312a5cd36c4c02ad242
pat <pat.liang@alum.utoronto.ca>
2022-11-24 10:06:45 -0800

    Added Manish username

--- src/main/java/com/battlesnake/starter/Snake.java
+++ src/main/java/com/battlesnake/starter/Snake.java
@@ -98,7 +98,7 @@ public class Snake {
         public Map<String, String> index() {
             Map<String, String> response = new HashMap<>();
             response.put("apiversion", "1");
-            response.put("author", ""); // TODO: Your Battlesnake Username
+            response.put("author", "Manish"); // TODO: Your Battlesnake Username
             response.put("color", "#00FF00"); // TODO: Personalize
             response.put("head", "default"); // TODO: Personalize
             response.put("tail", "default"); // TODO: Personalize
